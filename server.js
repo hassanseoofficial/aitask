94be3c99ee76eb666a48d300e5985058c359501d
ALi Hassan <alihassan10.cs@gmail.com>
2024-12-21 20:13:57 +0500

    intialize

---
+++ server.js
@@ -0,0 +1,6 @@
+// Add these lines after the other middleware
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
